cb0e72b0bdf538a84bd84116f68ea36284f3e749
cubajazz <cubajazz@gmail.com>
2021-12-07 00:41:00 +0100

    Update generate-qr.sh

--- generate-qr.sh
+++ generate-qr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function generate_qr(){
   input_txt="$1"
-  output_png="${input_txt/.txt/.png}"
+  output_png="${cubajazz_txt/.cubajazz/.png}"
   echo "Output: $output_png"
   cat "$input_txt" | tr -d '\n' | qrencode -t png -o "$output_png"
 }
